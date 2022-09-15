712f2eea26b5e3657a43e150866b3dc98c592845
qasemB <mahdicmptr@gmail.com>
2022-09-15 20:03:02 +0430

    part #11

--- assets/js/users.js
+++ assets/js/users.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, {});
+
+
+    const addUserButton = document.getElementById('add_user_button')
+    addUserButton.addEventListener('click', ()=>{
+        document.querySelector('.add_user_modal').classList.add('show')
+        document.querySelector('.add_user_modal_back').classList.remove('dis-none')
+    })
+
+    document.querySelector('.add_user_modal_back').addEventListener('click', (e)=>{
+        document.querySelector('.add_user_modal').classList.remove('show')
+        e.target.classList.add('dis-none')
+    })
+
 });
