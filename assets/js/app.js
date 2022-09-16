41018b0930d4fab77fcc0427594f778dc15f07a3
qasemB <mahdicmptr@gmail.com>
2022-09-17 02:07:23 +0430

    part #12

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {});
+    var instances = M.Sidenav.init(elems, {
+        edge: "right"
+    });
 });
