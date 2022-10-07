9726ac5c14c4b92c5fcd32af248e713502670ae7
qasemB <mahdicmptr@gmail.com>
2022-10-07 14:55:45 +0330

    part #17

--- assets/js/users.js
+++ assets/js/users.js
@@ -14,40 +14,40 @@ document.addEventListener('DOMContentLoaded', function() {
         e.target.classList.add('dis-none')
     })
 
-    setTimeout(function(){
-        console.log("start...");
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then((response) => response.json())
-        .then((json) => {
-            console.log("end...");
-            console.log(json)
+    // setTimeout(function(){
+    //     console.log("start...");
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then((response) => response.json())
+    //     .then((json) => {
+    //         console.log("end...");
+    //         console.log(json)
 
-            let html = ""
+    //         let html = ""
 
-            for (const user of json) {
-                html = html + `<tr>
-                                    <td>${user.id}</td>
-                                    <td>${user.name}</td>
-                                    <td>${user.username}</td>
-                                    <td>${user.email}</td>
-                                    <td>${user.address.city}</td>
-                                    <td>
-                                    <i class="material-icons orange-text darken-3 m-l-5 m-r-5 hov-pointer">edit</i>
-                                    <i class="material-icons red-text m-l-5 m-r-5 hov-pointer">
-                                        delete_forever
-                                    </i>
-                                    </td>
-                                </tr>`
-            }
+    //         for (const user of json) {
+    //             html = html + `<tr>
+    //                                 <td>${user.id}</td>
+    //                                 <td>${user.name}</td>
+    //                                 <td>${user.username}</td>
+    //                                 <td>${user.email}</td>
+    //                                 <td>${user.address.city}</td>
+    //                                 <td>
+    //                                 <i class="material-icons orange-text darken-3 m-l-5 m-r-5 hov-pointer">edit</i>
+    //                                 <i class="material-icons red-text m-l-5 m-r-5 hov-pointer">
+    //                                     delete_forever
+    //                                 </i>
+    //                                 </td>
+    //                             </tr>`
+    //         }
 
-            document.querySelector('.desktop_table_body').innerHTML = html
+    //         document.querySelector('.desktop_table_body').innerHTML = html
 
 
             
 
 
-        });
-    },5000)
+    //     });
+    // },5000)
 
 
 
