9253335ddab93124cd6836a87b720619d433d2de
qasemB <mahdicmptr@gmail.com>
2022-10-14 19:32:54 +0330

    part #21

--- assets/js/alpineJsContollers/usersController.js
+++ assets/js/alpineJsContollers/usersController.js
@@ -1,15 +1,14 @@
 document.addEventListener('alpine:init', () => {
-    Alpine.data('usersData', function(){
-        return {
-            users: [],
-            isLoading: false,
-            getUsers(){
-                this.isLoading = true
-                axios.get("https://jsonplaceholder.typicode.com/users").then((res)=>{
-                    this.users = res.data
-                    this.isLoading = false
-                })
-            }
+    Alpine.data('usersData', ()=>({
+        users: [],
+        isLoading: false,
+        getUsers(){
+            this.isLoading = true
+            axios.get("https://jsonplaceholder.typicode.com/users").then((res)=>{
+                this.users = res.data
+            }).finally(()=>{
+                this.isLoading = false
+            })
         }
-    })
+    }))
 })
